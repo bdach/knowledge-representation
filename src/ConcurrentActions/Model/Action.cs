b360c21497e9f8ff5b88bbad2df09d6584f6e536
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2018-04-21 23:35:51 +0200

    Add basic models
    * Add classes for actions, fluents and states
    * Add unit test project

---
+++ src/ConcurrentActions/Model/Action.cs
@@ -0,0 +1,28 @@
+﻿namespace Model
+{
+    /// <summary>
+    /// Represents a single action in the dynamic system.
+    /// An action represents some kind of activity in the system being described, with certain or uncertain effects.
+    /// </summary>
+    public class Action
+    {
+        /// <summary>
+        /// Name of the action.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Action"/> class with the supplied name.
+        /// </summary>
+        /// <param name="name">The name of the action.</param>
+        public Action(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ src/ConcurrentActions/Model/Fluent.cs
@@ -0,0 +1,28 @@
+﻿namespace Model
+{
+    /// <summary>
+    /// Represents a single fluent in the dynamic system.
+    /// A fluent can be equated to a binary state of a particular element of the language signature.
+    /// </summary>
+    public class Fluent
+    {
+        /// <summary>
+        /// Name of the fluent.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Fluent"/> class with a specified name.
+        /// </summary>
+        /// <param name="name">The name of the fluent.</param>
+        public Fluent(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
