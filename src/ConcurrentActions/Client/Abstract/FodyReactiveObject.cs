589c5830a9a91e4180a6935b059acf44f64d0dcb
Tymon Felski <tymon.felski@gmail.com>
2018-04-25 00:06:27 +0200

    Successfully configured Fody and ReactiveUI, prepared AppBootstrapper

---
+++ src/ConcurrentActions/Client/Abstract/FodyReactiveObject.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+
+namespace Client.Abstract
+{
+    /// <summary>
+    /// <see cref="ReactiveObject"/> wrapper allowing Fody to seamlessly inject event calls into property setters.
+    /// </summary>
+    public abstract class FodyReactiveObject : ReactiveObject
+    {
+        /// <summary>
+        /// Custom event invoker required to make Fody work due to ReactiveUI hiding its own calls.
+        /// </summary>
+        protected void OnPropertyChanged(string propertyName)
+        {
+            this.RaisePropertyChanged(propertyName);
+        }
+
+        /// <summary>
+        /// Custom event invoker required to make Fody work due to ReactiveUI hiding its own calls.
+        /// </summary>
+        protected void OnPropertyChanging(string propertyName)
+        {
+            this.RaisePropertyChanging(propertyName);
+        }
+    }
+}
--- src/ConcurrentActions/Client/App.xaml.cs
+++ src/ConcurrentActions/Client/App.xaml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using Client.Providers;
+using ReactiveUI;
 
 namespace Client
 {
@@ -13,5 +9,27 @@ namespace Client
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Instance of <see cref="AppBootstrapper"/> class used to manage
+        /// the initialization of the application and dependency injections.
+        /// </summary>
+        public static AppBootstrapper Bootstrapper { get; set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public App()
+        {
+            Bootstrapper = new AppBootstrapper();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // TODO: remove this if it's not going to be useful
+            // Fix for multiple notifications firing ReactiveList.AddRange
+            RxApp.SupportsRangeNotifications = false;
+        }
     }
 }
