ce3306b11d27bdd4d6fdafff679d8b0f209dd313
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2018-05-02 15:38:31 +0200

    Add support for adding formulae in queries

--- src/ConcurrentActions/Client/Interface/IQueryClauseViewModel.cs
+++ src/ConcurrentActions/Client/Interface/IQueryClauseViewModel.cs
@@ -13,7 +13,7 @@ namespace Client.Interface
         /// <summary>
         /// Adds a new <see cref="IViewModelFor{IFormula}"/> to edited clause.
         /// </summary>
-        ReactiveCommand<IFormulaViewModel, Unit> AddFormula { get; }
+        ReactiveCommand<IFormulaViewModel, IFormulaViewModel> AddFormula { get; }
 
         /// <summary>
         /// Adds a new <see cref="ProgramViewModel"/> to edited clause.
