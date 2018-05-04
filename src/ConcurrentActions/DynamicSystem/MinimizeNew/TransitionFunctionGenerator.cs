3b7353ea2576ec9c7d8020e82930792b0f3c6156
Michał Kołodziej <kolodziejm355@gmail.com>
2018-05-04 18:07:49 +0200

    created templates

---
+++ src/ConcurrentActions/DynamicSystem/MinimizeNew/TransitionFunctionGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace DynamicSystem.MinimizeNew
+{
+    /// <summary>
+    /// Class for generating transition function
+    /// </summary>
+    class TransitionFunctionGenerator
+    {
+        public TransitionFunction GenerateTransitionFunction(Dictionary<(State, CompoundAction), HashSet<State>> res_0, Dictionary<(CompoundAction, State, State), HashSet<Fluent>> newSet)
+        {
+            //TODO: implement
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ConcurrentActions/DynamicSystem/QueriesEvaluation/QueriesEvaluator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using Model.QueryLanguage;
+
+namespace DynamicSystem.QueriesEvaluation
+{
+    /// <summary>
+    /// Class for evaluating queries for all domain models.
+    /// </summary>
+    public static class QueriesEvaluator
+    {
+        //TODO: change return type
+        public static void Evaluate(IEnumerable<State> initialStates, QuerySet queries, TransitionFunction transitionFunction)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
