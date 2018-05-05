5dd6f43eb20ba77db7d981f3333a3c8269befbbe
Mateusz Rymuszka <mattrym@gmail.com>
2018-05-05 20:46:55 +0200

    Added TransitionFunctionGenerator documentation

--- src/ConcurrentActions/DynamicSystem/MinimizeNew/TransitionFunctionGenerator.cs
+++ src/ConcurrentActions/DynamicSystem/MinimizeNew/TransitionFunctionGenerator.cs
@@ -12,6 +12,14 @@ namespace DynamicSystem.MinimizeNew
     /// </summary>
     class TransitionFunctionGenerator
     {
+
+        /// <summary>
+        /// Generates <see cref="TransitionFunction"/> by finding states belonging to output of Res_0 function 
+        /// that minimalize New set of inertial fluent that can change with action execution.
+        /// </summary>
+        /// <param name="resZero"><see cref="Dictionary{ValueTuple{CompoundAction, State}, HashSet{State}"/> describing Res_0 function.</param>
+        /// <param name="newSets"><see cref="Dictionary{ValueTuple{CompoundAction, State, State}, HashSet{Fluent}}"/> describing New sets.</param>
+        /// <returns><see cref="TransitionFunction"/> object.</returns>
         public TransitionFunction GenerateTransitionFunction(
             Dictionary<(CompoundAction, State), HashSet<State>> resZero,
             Dictionary<(CompoundAction, State, State), HashSet<Fluent>> newSets)
@@ -23,7 +31,7 @@ namespace DynamicSystem.MinimizeNew
                 var compoundAction = assignment.Key.Item1;
                 var state = assignment.Key.Item2;
                 var potentialResults = assignment.Value;
-        
+
                 var consideredNewSets = FindConsideredNewSets(compoundAction, state, potentialResults, newSets);
 
                 transitionFunction[compoundAction, state] =
@@ -32,6 +40,11 @@ namespace DynamicSystem.MinimizeNew
             return transitionFunction;
         }
 
+        /// <summary>
+        /// Initializes <see cref="TransitionFunction"/> with compound actions and states which are arguments of Res_0 function.
+        /// </summary>
+        /// <param name="resZero"><see cref="Dictionary{ValueTuple{CompoundAction, State}, HashSet{State}"/> describing Res_0 function.</param>
+        /// <returns>Empty <see cref="TransitionFunction"/>.</returns>
         private static TransitionFunction InitializeTransitionFunction(Dictionary<(CompoundAction, State), HashSet<State>> resZero)
         {
             var compoundActions = resZero.Keys
@@ -44,20 +57,35 @@ namespace DynamicSystem.MinimizeNew
             return new TransitionFunction(compoundActions, states);
         }
 
+        /// <summary>
+        /// Filter New <see cref="HashSet{T}"/> of <see cref="State"/> that will be considered for specific <see cref="CompoundAction"/> and <see cref="State"/>
+        /// </summary>
+        /// <param name="compoundAction"><see cref="CompoundAction"/> to be executed.</param>
+        /// <param name="state">Current <see cref="State"/> of a dynamic system.</param>
+        /// <param name="potentialResults"><see cref="HashSet{StateT}"/> with possible results of compound action execution.</param>
+        /// <param name="newSets"><see cref="Dictionary{ValueTuple{CompoundAction, State, State}, HashSet{Fluent}}"/> describing New sets.</param>
+        /// <returns><see cref="Dictionary{ValueTuple{CompoundAction, State, State}, HashSet{Fluent}}"/> containing New sets that will be considered.</returns>
         private static Dictionary<(CompoundAction, State, State), HashSet<Fluent>> FindConsideredNewSets(
             CompoundAction compoundAction, State state, HashSet<State> potentialResults,
             Dictionary<(CompoundAction, State, State), HashSet<Fluent>> newSets)
         {
             return newSets
-                .Where(newSet => 
+                .Where(newSet =>
                        compoundAction == newSet.Key.Item1
                     && state == newSet.Key.Item2
                     && potentialResults.Contains(newSet.Key.Item3))
                 .ToDictionary(newSet => newSet.Key, newSet => newSet.Value);
         }
 
+        /// <summary>
+        /// Generates transition function for specific compound action and state by finding states belonging to output of Res_0 function 
+        /// that minimalize New set of inertial fluent that can change with action execution.
+        /// </summary>
+        /// <param name="assignment"><see cref="ValueTuple{CompoundAction, State, HashSet{State}}"/> describing Res_0 assignment.</param>
+        /// <param name="newSetDict"><see cref="Dictionary{ValueTuple{CompoundAction, State, State}, HashSet{Fluent}}"/> with New sets that are considered.</param>
+        /// <returns></returns>
         private HashSet<State> GenerateTransitionFunction(
-            (CompoundAction, State, HashSet<State>) assignment, 
+            (CompoundAction, State, HashSet<State>) assignment,
             Dictionary<(CompoundAction, State, State), HashSet<Fluent>> newSetDict)
         {
             var compoundAction = assignment.Item1;
