805b7b4ab4ea363be8cd6e39bf93e316e3c1d3ba
Mateusz Rymuszka <mattrym@gmail.com>
2018-05-05 19:42:55 +0200

    Implemented generation of transition function

--- src/ConcurrentActions/DynamicSystem/MinimizeNew/TransitionFunctionGenerator.cs
+++ src/ConcurrentActions/DynamicSystem/MinimizeNew/TransitionFunctionGenerator.cs
@@ -12,10 +12,68 @@ namespace DynamicSystem.MinimizeNew
     /// </summary>
     class TransitionFunctionGenerator
     {
-        public TransitionFunction GenerateTransitionFunction(Dictionary<(State, CompoundAction), HashSet<State>> res_0, Dictionary<(CompoundAction, State, State), HashSet<Fluent>> newSet)
+        public TransitionFunction GenerateTransitionFunction(
+            Dictionary<(CompoundAction, State), HashSet<State>> resZero,
+            Dictionary<(CompoundAction, State, State), HashSet<Fluent>> newSets)
         {
-            //TODO: implement
-            throw new NotImplementedException();
+            var transitionFunction = InitializeTransitionFunction(resZero);
+
+            foreach (var assignment in resZero)
+            {
+                var compoundAction = assignment.Key.Item1;
+                var state = assignment.Key.Item2;
+                var potentialResults = assignment.Value;
+        
+                var consideredNewSets = FindConsideredNewSets(compoundAction, state, potentialResults, newSets);
+
+                transitionFunction[compoundAction, state] =
+                    GenerateTransitionFunction((compoundAction, state, potentialResults), consideredNewSets);
+            }
+            return transitionFunction;
+        }
+
+        private static TransitionFunction InitializeTransitionFunction(Dictionary<(CompoundAction, State), HashSet<State>> resZero)
+        {
+            var compoundActions = resZero.Keys
+                .Select(key => key.Item1)
+                .ToArray();
+            var states = resZero.Keys
+                .Select(key => key.Item2)
+                .ToArray();
+
+            return new TransitionFunction(compoundActions, states);
+        }
+
+        private static Dictionary<(CompoundAction, State, State), HashSet<Fluent>> FindConsideredNewSets(
+            CompoundAction compoundAction, State state, HashSet<State> potentialResults,
+            Dictionary<(CompoundAction, State, State), HashSet<Fluent>> newSets)
+        {
+            return newSets
+                .Where(newSet => 
+                       compoundAction == newSet.Key.Item1
+                    && state == newSet.Key.Item2
+                    && potentialResults.Contains(newSet.Key.Item3))
+                .ToDictionary(newSet => newSet.Key, newSet => newSet.Value);
+        }
+
+        private HashSet<State> GenerateTransitionFunction(
+            (CompoundAction, State, HashSet<State>) assignment, 
+            Dictionary<(CompoundAction, State, State), HashSet<Fluent>> newSetDict)
+        {
+            var compoundAction = assignment.Item1;
+            var state = assignment.Item2;
+            var potentialResults = assignment.Item3;
+
+            var newSets = newSetDict.Values;
+            var minimalizingStates = new HashSet<State>();
+
+            foreach (var result in potentialResults)
+            {
+                var resultNewSet = newSetDict[(compoundAction, state, result)];
+                if (!newSets.Any(newSet => newSet.IsProperSubsetOf(resultNewSet)))
+                    minimalizingStates.Add(result);
+            }
+            return minimalizingStates;
         }
     }
 }
