1a220ce0346fd245d7b02c5b1d71b90779ca7f7b
jedrek <jedrek@fijalkowscy.com>
2018-05-06 13:47:48 +0200

    Docs

--- src/ConcurrentActions/DynamicSystem/NewGeneration/NewGenerator.cs
+++ src/ConcurrentActions/DynamicSystem/NewGeneration/NewGenerator.cs
@@ -10,11 +10,19 @@ using Action = Model.Action;
 
 namespace DynamicSystem.NewGeneration
 {
+    /// <summary>
+    /// Class for generating sets of literals for New.
+    /// </summary>
     public class NewGenerator
     {
         private ActionDomain _actionDomain;
         private HashSet<Fluent> _fluents;
 
+        /// <summary>
+        /// Initializes an instance of the <see cref="NewGenerator"/> class.
+        /// </summary>
+        /// <param name="actionDomain"><see cref="ActionDomain"/> containing fluent release statements.</param>
+        /// <param name="fluents">Set of <see cref="Fluent"/>.</param>
         public NewGenerator(ActionDomain actionDomain, HashSet<Fluent> fluents)
         {
             _actionDomain = actionDomain;
@@ -24,30 +32,37 @@ namespace DynamicSystem.NewGeneration
         /// <summary>
         /// Generates New, a set of literals,
         /// where either the inertial fluents changed values between states
-        /// or they where released
+        /// or they where released.
         /// </summary>
-        /// <param name="action">Action</param>
-        /// <param name="from">State</param>
-        /// <param name="to">Next state</param>
-        /// <returns>Set of literals</returns>
+        /// <param name="action"><see cref="Action"/></param>
+        /// <param name="from"><see cref="State"/></param>
+        /// <param name="to">Next <see cref="State"/></param>
+        /// <returns>Set of <see cref="Literal"/></returns>
         public IEnumerable<Literal> GetLiterals(Action action, State from, State to)
         {
             var releaseStatements = _actionDomain.FluentReleaseStatements
               .Where(e => e.Action.Equals(action) && e.Precondition.Evaluate(from)).ToList();
 
-            // for noninertial add literal || for inertial see if value changed
             var literals = _fluents.Where(f => releaseStatements.Any(e => e.Fluent.Equals(f)) || !from[f].Equals(to[f]))
                 .Select(f => new Literal(f, !to[f]));
 
             return literals;
         }
 
+        /// <summary>
+        /// Generates New, a set of literals, 
+        /// where either the inertial fluents changed values between states
+        /// or they where released.
+        /// </summary>
+        /// <param name="actions">Set of <see cref="Action"/></param>
+        /// <param name="from"><see cref="State"/></param>
+        /// <param name="to">Next <see cref="State"/></param>
+        /// <returns>Set of <see cref="Literal"/></returns>
         public IEnumerable<Literal> GetLiterals(IEnumerable<Action> actions, State from, State to)
         {
             var releaseStatements = _actionDomain.FluentReleaseStatements
               .Where(e => actions.Contains(e.Action) && e.Precondition.Evaluate(from)).ToList();
 
-            // for noninertial add literal || for inertial see if value changed
             var literals = _fluents.Where(f => releaseStatements.Any(e => e.Fluent.Equals(f)) || !from[f].Equals(to[f]))
                 .Select(f => new Literal(f, !to[f]));
 
